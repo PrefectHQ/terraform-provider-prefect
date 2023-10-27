e76bb20bbb3ae51b17bb9da7d10ae407456cea57
Edward Park <ed.sh.park@gmail.com>
2023-10-27 09:29:22 -0700

    oops

--- examples/workspace_access.tf
+++ examples/workspace_access.tf
@@ -8,7 +8,7 @@ resource "prefect_service_account" "bot" {
 	name = "a-cool-bot"
 }
 resource "prefect_workspace_access" "bot_access" {
-	accessor_type = "SERVICE ACCOUNT"
+	accessor_type = "SERVICE_ACCOUNT"
 	accessor_id = prefect_service_account.bot.id
 	workspace_id = data.prefect_workspace.prd.id
 	workspace_role_id = data.prefect_workspace_role.developer.id
