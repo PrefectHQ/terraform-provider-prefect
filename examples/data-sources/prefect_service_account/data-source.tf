31e1f39b26dec2e4c225302db82674ba9ca9a0e0
Edward Park <ed.sh.park@gmail.com>
2023-11-03 16:13:27 -0700

    docs

--- examples/data-sources/prefect_service_account/data-source.tf
+++ examples/data-sources/prefect_service_account/data-source.tf
@@ -1,3 +1,9 @@
-data "prefect_service_account" "my_existing_bot" {
-  id = "service-acount-UUID"
+data "prefect_service_account" "bot" {
+  id = "7de0291d-59d0-4d57-a629-fe47960aa61b"
+}
+
+# or
+
+data "prefect_service_account" "bot" {
+  name = "my-bot-name"
 }
