19e4451bbff455f09091080b8fd739a77a8e441f
Jamie Zieziula <jamie.m.dick@gmail.com>
2024-02-07 11:18:05 -0500

    fix: minor typo in `account` data source docs (#142)
    * fix typo
    
    * Generate Terraform Docs
    
    * underscore
    
    * underscore
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/data-sources/prefect_account/data-source.tf
+++ examples/data-sources/prefect_account/data-source.tf
@@ -4,4 +4,4 @@ data "prefect_account" "my_organization" {
 }
 
 # Implicit account read, using the account ID from the provider
-data "prefect-account" "my_organization" {}
+data "prefect_account" "my_organization" {}
