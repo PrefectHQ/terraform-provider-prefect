422a64cc228a2448e051021ae9edf1d88500a9a9
Richard Weerasinghe <richard@skyscrapr.io>
2024-04-17 21:22:02 +1200

    add support for deployment access

--- internal/provider/resources/deployment_access.go
+++ internal/provider/resources/deployment_access.go
@@ -148,17 +148,17 @@ func (r *DeploymentAccessResource) Schema(_ context.Context, _ resource.SchemaRe
 					"manage_actor_ids": schema.ListAttribute{
 						Description: "The list of actor IDs to grant manage access to.",
 						ElementType: types.StringType,
-						Required:    true,
+						Optional:    true,
 					},
 					"run_actor_ids": schema.ListAttribute{
 						Description: "The list of actor IDs to grant run access to.",
 						ElementType: types.StringType,
-						Required:    true,
+						Optional:    true,
 					},
 					"view_actor_ids": schema.ListAttribute{
 						Description: "The list of actor IDs to grant view access to.",
 						ElementType: types.StringType,
-						Required:    true,
+						Optional:    true,
 					},
 					"manage_team_ids": schema.ListAttribute{
 						Description: "The list of team IDs to grant manage access to.",
