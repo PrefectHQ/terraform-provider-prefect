f55b609c90b322753537694efefade56742c8be8
Edward Park <ed.sh.park@gmail.com>
2024-05-03 17:01:45 -0700

    fix: team_access endpoint is a PUT (#170)
    * fix: team_access endpoint is a PUT
    
    * words
    
    * add acceptance test for team access
    
    * oops
    
    * list
    
    * oops
    
    * bah
    
    * fix
    
    * fix delete
    
    * maybe
    
    * final

--- internal/client/workspace_access.go
+++ internal/client/workspace_access.go
@@ -45,29 +45,45 @@ func (c *Client) WorkspaceAccess(accountID uuid.UUID, workspaceID uuid.UUID) (ap
 
 // Upsert creates or updates access to a workspace for various accessor types.
 func (c *WorkspaceAccessClient) Upsert(ctx context.Context, accessorType string, accessorID uuid.UUID, roleID uuid.UUID) (*api.WorkspaceAccess, error) {
-	payload := api.WorkspaceAccessUpsert{
-		WorkspaceRoleID: roleID,
+	// NOTE: our access APIs can optionally take a single "access" payload
+	// or a list of them. In our case, we'll always pass a 1-item slice
+	payloads := []api.WorkspaceAccessUpsert{
+		{
+			WorkspaceRoleID: roleID,
+		},
 	}
 	var requestPath string
+
+	// NOTE: this is a quirk of our <entity>_access API at the moment
+	// where user_access and bot_access were originally set up as a POST.
+	//
+	// Semantically, they should be a PUT, which the newer team_access API is set up as.
+	// At a later point, we will migrate the user/bot API variants over to a PUT
+	// in a breaking change.
+	var requestMethod string
+
 	if accessorType == utils.User {
 		requestPath = fmt.Sprintf("%s/user_access/", c.routePrefix)
-		payload.UserID = &accessorID
+		payloads[0].UserID = &accessorID
+		requestMethod = http.MethodPost
 	}
 	if accessorType == utils.ServiceAccount {
 		requestPath = fmt.Sprintf("%s/bot_access/", c.routePrefix)
-		payload.BotID = &accessorID
+		payloads[0].BotID = &accessorID
+		requestMethod = http.MethodPost
 	}
 	if accessorType == utils.Team {
 		requestPath = fmt.Sprintf("%s/team_access/", c.routePrefix)
-		payload.TeamID = &accessorID
+		payloads[0].TeamID = &accessorID
+		requestMethod = http.MethodPut
 	}
 
 	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(&payload); err != nil {
+	if err := json.NewEncoder(&buf).Encode(&payloads); err != nil {
 		return nil, fmt.Errorf("failed to encode upsert payload data: %w", err)
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, requestPath, &buf)
+	req, err := http.NewRequestWithContext(ctx, requestMethod, requestPath, &buf)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
@@ -85,28 +101,44 @@ func (c *WorkspaceAccessClient) Upsert(ctx context.Context, accessorType string,
 		return nil, fmt.Errorf("status code %s, error=%s", resp.Status, errorBody)
 	}
 
-	var workspaceAccess api.WorkspaceAccess
-	if err := json.NewDecoder(resp.Body).Decode(&workspaceAccess); err != nil {
+	var workspaceAccesses []api.WorkspaceAccess
+	if err := json.NewDecoder(resp.Body).Decode(&workspaceAccesses); err != nil {
 		return nil, fmt.Errorf("failed to decode response: %w", err)
 	}
 
-	return &workspaceAccess, nil
+	return &workspaceAccesses[0], nil
 }
 
 // Get fetches workspace access for various accessor types via accessID.
 func (c *WorkspaceAccessClient) Get(ctx context.Context, accessorType string, accessID uuid.UUID) (*api.WorkspaceAccess, error) {
 	var requestPath string
+	var requestMethod string
+
+	// NOTE: this is a quirk of our <entity>_access API at the moment
+	// where user_access and bot_access can be fetched individually by resource ID,
+	// whereas team_access resources must be fetched as a list, scoped to the workspace.
+	//
+	// Here, we'll key off of the `accessorType` to determine the correct API endpoint,
+	// and we'll conditionally handle unmarshalling the response to
+	// a single WorkspaceAccess (for user_access and bot_access)
+	// or a list of them (for team_access) -> filtering for the passed `accessID`.
 	if accessorType == utils.User {
+		// GET: /.../<workspace_access_id>
+		requestMethod = http.MethodGet
 		requestPath = fmt.Sprintf("%s/user_access/%s", c.routePrefix, accessID.String())
 	}
 	if accessorType == utils.ServiceAccount {
+		// GET: /.../<workspace_access_id>
+		requestMethod = http.MethodGet
 		requestPath = fmt.Sprintf("%s/bot_access/%s", c.routePrefix, accessID.String())
 	}
 	if accessorType == utils.Team {
-		requestPath = fmt.Sprintf("%s/team_access/%s", c.routePrefix, accessID.String())
+		// POST: /.../filter
+		requestMethod = http.MethodPost
+		requestPath = fmt.Sprintf("%s/team_access/filter", c.routePrefix)
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, requestPath, http.NoBody)
+	req, err := http.NewRequestWithContext(ctx, requestMethod, requestPath, http.NoBody)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
@@ -125,24 +157,59 @@ func (c *WorkspaceAccessClient) Get(ctx context.Context, accessorType string, ac
 	}
 
 	var workspaceAccess api.WorkspaceAccess
-	if err := json.NewDecoder(resp.Body).Decode(&workspaceAccess); err != nil {
-		return nil, fmt.Errorf("failed to decode response: %w", err)
+	var workspaceAccesses []api.WorkspaceAccess
+
+	// If this is a team_access resource, we'll expect a list of WorkspaceAccess objects
+	if accessorType == utils.Team {
+		if err := json.NewDecoder(resp.Body).Decode(&workspaceAccesses); err != nil {
+			return nil, fmt.Errorf("failed to decode response: %w", err)
+		}
+	}
+	for _, access := range workspaceAccesses {
+		if access.ID == accessID {
+			workspaceAccess = access
+
+			break
+		}
+	}
+
+	// Otherwise, we'll expect a single WorkspaceAccess object, fetched by `accessID`
+	if accessorType == utils.User || accessorType == utils.ServiceAccount {
+		if err := json.NewDecoder(resp.Body).Decode(&workspaceAccess); err != nil {
+			return nil, fmt.Errorf("failed to decode response: %w", err)
+		}
+	}
+
+	if workspaceAccess.ID == uuid.Nil {
+		return nil, fmt.Errorf("workspace access not found for accessID: %s", accessID.String())
 	}
 
 	return &workspaceAccess, nil
 }
 
 // DeleteUserAccess deletes a service account's workspace access via accessID.
-func (c *WorkspaceAccessClient) Delete(ctx context.Context, accessorType string, accessID uuid.UUID) error {
+func (c *WorkspaceAccessClient) Delete(ctx context.Context, accessorType string, accessID uuid.UUID, accessorID uuid.UUID) error {
 	var requestPath string
+
+	// NOTE: this is a quirk of our <entity>_access API at the moment
+	// where user_access and bot_access can be deleted individually by the `accessID`
+	// whereas team_access resources must be deleted by the `accessorID`, eg. the team_id
+	//
+	// Here, we'll key off of the `accessorType` to determine the correct URL parameter to use.
+	//
+	// DELETE: /.../<workspace_access_id>
 	if accessorType == utils.User {
 		requestPath = fmt.Sprintf("%s/user_access/%s", c.routePrefix, accessID.String())
 	}
+
+	// DELETE: /.../<workspace_access_id>
 	if accessorType == utils.ServiceAccount {
 		requestPath = fmt.Sprintf("%s/bot_access/%s", c.routePrefix, accessID.String())
 	}
+
+	// DELETE: /.../<team_id>
 	if accessorType == utils.Team {
-		requestPath = fmt.Sprintf("%s/team_access/%s", c.routePrefix, accessID.String())
+		requestPath = fmt.Sprintf("%s/team_access/%s", c.routePrefix, accessorID.String())
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, requestPath, http.NoBody)
