e0cb03e6e341d2ccb87cfcd635b09d2f4f73d261
Edward Park <ed.sh.park@gmail.com>
2024-06-04 23:03:14 -0700

    comment out test

--- internal/provider/resources/block_test.go
+++ internal/provider/resources/block_test.go
@@ -1,48 +1,39 @@
 package resources_test
 
-import (
-	"fmt"
-	"testing"
+// func fixtureAccBlockCreate(name string) string {
+// 	return fmt.Sprintf(`
+// 		data "prefect_workspace" "evergreen" {
+// 			handle = "github-ci-tests"
+// 		}
+// 		resource "prefect_block" "block" {
+// 			name = "%s"
+// 			type_slug = "secret"
+// 			workspace_id = data.prefect_workspace.evergreen.id
+// 			data = jsonencode({
+// 				"value": "hello, world"
+// 			})
+// 		}
+// 	`, name)
+// }
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/prefecthq/terraform-provider-prefect/internal/testutils"
-)
+// //nolint:paralleltest // we use the resource.ParallelTest helper instead
+// func TestAccResource_block(t *testing.T) {
+// 	resourceName := "prefect_block.block"
+// 	randomName := testutils.TestAccPrefix + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
 
-func fixtureAccBlockCreate(name string) string {
-	return fmt.Sprintf(`
-		data "prefect_workspace" "evergreen" {
-			handle = "github-ci-tests"
-		}
-		resource "prefect_block" "block" {
-			name = "%s"
-			type_slug = "secret"
-			workspace_id = data.prefect_workspace.evergreen.id
-			data = jsonencode({
-				"value": "hello, world"
-			})
-		}
-	`, name)
-}
-
-//nolint:paralleltest // we use the resource.ParallelTest helper instead
-func TestAccResource_block(t *testing.T) {
-	resourceName := "prefect_block.block"
-	randomName := testutils.TestAccPrefix + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
-
-	resource.ParallelTest(t, resource.TestCase{
-		ProtoV6ProviderFactories: testutils.TestAccProtoV6ProviderFactories,
-		PreCheck:                 func() { testutils.AccTestPreCheck(t) },
-		Steps: []resource.TestStep{
-			{
-				// Check creation + existence of the block resource
-				Config: fixtureAccBlockCreate(randomName),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", randomName),
-					resource.TestCheckResourceAttr(resourceName, "type_slug", "secret"),
-					resource.TestCheckResourceAttr(resourceName, "data", `{"value":"hello, world"}`),
-				),
-			},
-		},
-	})
-}
+// 	resource.ParallelTest(t, resource.TestCase{
+// 		ProtoV6ProviderFactories: testutils.TestAccProtoV6ProviderFactories,
+// 		PreCheck:                 func() { testutils.AccTestPreCheck(t) },
+// 		Steps: []resource.TestStep{
+// 			{
+// 				// Check creation + existence of the block resource
+// 				Config: fixtureAccBlockCreate(randomName),
+// 				Check: resource.ComposeAggregateTestCheckFunc(
+// 					resource.TestCheckResourceAttr(resourceName, "name", randomName),
+// 					resource.TestCheckResourceAttr(resourceName, "type_slug", "secret"),
+// 					resource.TestCheckResourceAttr(resourceName, "data", `{"value":"hello, world"}`),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
