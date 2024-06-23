8f73df8fd1dc31d5c29778d2a7e40c6e965d8fde
Richard Weerasinghe <richard@skyscrapr.io>
2024-06-24 09:59:59 +1000

    fixup import for deployments

--- internal/provider/resources/deployment.go
+++ internal/provider/resources/deployment.go
@@ -496,8 +496,8 @@ func (r *DeploymentResource) Delete(ctx context.Context, req resource.DeleteRequ
 // ImportState imports the resource into Terraform state.
 func (r *DeploymentResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	// we'll allow input values in the form of:
-	// - "workspace_id,name"
-	// - "name"
+	// - "id,workspace_id"
+	// - "id"
 	maxInputCount := 2
 	inputParts := strings.Split(req.ID, ",")
 
@@ -505,7 +505,7 @@ func (r *DeploymentResource) ImportState(ctx context.Context, req resource.Impor
 	if len(inputParts) > maxInputCount {
 		resp.Diagnostics.AddError(
 			"Unexpected Import Identifier",
-			fmt.Sprintf("Expected a maximum of 2 import identifiers, in the form of `workspace_id,name`. Got %q", req.ID),
+			fmt.Sprintf("Expected a maximum of 2 import identifiers, in the form of `id,workspace_id`. Got %q", req.ID),
 		)
 
 		return
@@ -515,19 +515,60 @@ func (r *DeploymentResource) ImportState(ctx context.Context, req resource.Impor
 	if len(inputParts) == maxInputCount && (inputParts[0] == "" || inputParts[1] == "") {
 		resp.Diagnostics.AddError(
 			"Unexpected Import Identifier",
-			fmt.Sprintf("Expected non-empty import identifiers, in the form of `workspace_id,name`. Got %q", req.ID),
+			fmt.Sprintf("Expected non-empty import identifiers, in the form of `id,workspace_id`. Got %q", req.ID),
 		)
 
 		return
 	}
 
-	if len(inputParts) == maxInputCount {
-		workspaceID := inputParts[0]
-		id := inputParts[1]
+	identifier := inputParts[0]
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), identifier)...)
 
-		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("workspace_id"), workspaceID)...)
-		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), id)...)
-	} else {
-		resource.ImportStatePassthroughID(ctx, path.Root("name"), req, resp)
+	if len(inputParts) == 2 && inputParts[1] != "" {
+		workspaceID, err := uuid.Parse(inputParts[1])
+		if err != nil {
+			resp.Diagnostics.AddError("problem parsing workspace ID", "see details")
+			return
+		}
+		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("workspace_id"), workspaceID.String())...)
 	}
 }
+
+// // ImportState imports the resource into Terraform state.
+// func (r *DeploymentResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+// 	// we'll allow input values in the form of:
+// 	// - "workspace_id,name"
+// 	// - "name"
+// 	maxInputCount := 2
+// 	inputParts := strings.Split(req.ID, ",")
+
+// 	// eg. "foo,bar,baz"
+// 	if len(inputParts) > maxInputCount {
+// 		resp.Diagnostics.AddError(
+// 			"Unexpected Import Identifier",
+// 			fmt.Sprintf("Expected a maximum of 2 import identifiers, in the form of `workspace_id,name`. Got %q", req.ID),
+// 		)
+
+// 		return
+// 	}
+
+// 	// eg. ",foo" or "foo,"
+// 	if len(inputParts) == maxInputCount && (inputParts[0] == "" || inputParts[1] == "") {
+// 		resp.Diagnostics.AddError(
+// 			"Unexpected Import Identifier",
+// 			fmt.Sprintf("Expected non-empty import identifiers, in the form of `workspace_id,name`. Got %q", req.ID),
+// 		)
+
+// 		return
+// 	}
+
+// 	if len(inputParts) == maxInputCount {
+// 		workspaceID := inputParts[0]
+// 		id := inputParts[1]
+
+// 		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("workspace_id"), workspaceID)...)
+// 		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), id)...)
+// 	} else {
+// 		resource.ImportStatePassthroughID(ctx, path.Root("name"), req, resp)
+// 	}
+// }
