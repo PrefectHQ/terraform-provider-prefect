57b0f6591a4681c3d890c9a5f0464e454619bf24
Richard Weerasinghe <richard@skyscrapr.io>
2024-06-24 10:03:06 +1000

    fixup typo in flow resrource example

--- examples/resources/prefect_flow/import.sh
+++ examples/resources/prefect_flow/import.sh
@@ -1,2 +1,2 @@
 # Prefect Flows can be imported via flow_id,workspace_id
-terraform import prefect_variable.example 00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000
+terraform import prefect_flow.example 00000000-0000-0000-0000-000000000000,00000000-0000-0000-0000-000000000000
