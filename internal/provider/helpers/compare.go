b6dd0ca913038eff81b423587251bafa18f64a88
Mitchell Nielsen <mitchnielsen@users.noreply.github.com>
2024-08-23 11:26:32 -0500

    Remove extra space
    Co-authored-by: Edward Park <ed.sh.park@gmail.com>

--- internal/provider/helpers/compare.go
+++ internal/provider/helpers/compare.go
@@ -2,7 +2,7 @@ package helpers
 
 import "github.com/go-test/deep"
 
-// ObjectsEqual checks to see if two  objects are equivalent, accounting for
+// ObjectsEqual checks to see if two objects are equivalent, accounting for
 // differences in the order of the contents.
 func ObjectsEqual(obj1, obj2 interface{}) (bool, []string) {
 	differences := deep.Equal(obj1, obj2)
