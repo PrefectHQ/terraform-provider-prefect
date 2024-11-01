8457de32df36a3d4c9215ff57e0303345618f4ae
Mitchell Nielsen <inbox@mitchn.me>
2024-11-01 13:38:07 -0500

    Get tests working

--- internal/provider/resources/deployment_access_test.go
+++ internal/provider/resources/deployment_access_test.go
@@ -1,17 +1,20 @@
 package resources_test
 
 import (
+	"context"
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/prefecthq/terraform-provider-prefect/internal/api"
 	"github.com/prefecthq/terraform-provider-prefect/internal/provider/helpers"
 	"github.com/prefecthq/terraform-provider-prefect/internal/testutils"
 )
 
 type deploymentAccessConfig struct {
-	FlowName              string
-	DeploymentName        string
-	ServiceAccountName    string
 	WorkspaceResource     string
 	WorkspaceResourceName string
 }
@@ -20,34 +23,52 @@ func fixtureAccDeploymentAccess(cfg deploymentAccessConfig) string {
 	tmpl := `
 {{.WorkspaceResource}}
 
+data "prefect_workspace_role" "developer" {
+	name = "Developer"
+}
+
 resource "prefect_service_account" "test" {
-	name = "{{.ServiceAccountName}}"
+	name = "my-service-account"
+}
+
+resource "prefect_workspace_access" "test" {
+	accessor_type = "SERVICE_ACCOUNT"
+	accessor_id = prefect_service_account.test.id
+	workspace_role_id = data.prefect_workspace_role.developer.id
+	workspace_id = {{.WorkspaceResourceName}}.id
 }
 
 data "prefect_team" "test" {
 	name = "my-team"
 }
 
+resource "prefect_workspace_access" "test_team" {
+	accessor_type = "TEAM"
+	accessor_id = data.prefect_team.test.id
+	workspace_role_id = data.prefect_workspace_role.developer.id
+	workspace_id = {{.WorkspaceResourceName}}.id
+}
+
 resource "prefect_flow" "test" {
-	name = "{{.FlowName}}"
+	name = "my-flow"
 	workspace_id = {{.WorkspaceResourceName}}.id
 	tags = ["test"]
 }
 
 resource "prefect_deployment" "test" {
-	name = "{{.DeploymentName}}"
+	name = "my-deployment"
 	workspace_id = {{.WorkspaceResourceName}}.id
 	flow_id = prefect_flow.test.id
 }
 
 resource "prefect_deployment_access" "test" {
+	workspace_id = {{.WorkspaceResourceName}}.id
 	deployment_id = prefect_deployment.test.id
 
-	workspace_id = {{.WorkspaceResourceName}}.id
+	manage_actor_ids = [prefect_service_account.test.actor_id]
+	run_actor_ids = [prefect_service_account.test.actor_id]
+	view_actor_ids = [prefect_service_account.test.actor_id]
 
-	manage_actor_ids = [prefect_service_account.test.id]
-	run_actor_ids = [prefect_service_account.test.id]
-	view_actor_ids = [prefect_service_account.test.id]
 	manage_team_ids = [data.prefect_team.test.id]
 	run_team_ids = [data.prefect_team.test.id]
 	view_team_ids = [data.prefect_team.test.id]
@@ -59,20 +80,19 @@ resource "prefect_deployment_access" "test" {
 
 //nolint:paralleltest // we use the resource.ParallelTest helper instead
 func TestAccResource_deployment_access(t *testing.T) {
-	deploymentName := testutils.NewRandomPrefixedString()
-	serviceAccountName := testutils.NewRandomPrefixedString()
-	flowName := testutils.NewRandomPrefixedString()
-
 	workspace, workspaceName := testutils.NewEphemeralWorkspace()
 	workspaceResourceName := "prefect_workspace." + workspaceName
 
+	serviceAccountName := "my-service-account"
+	teamName := "my-team"
+
 	cfgSet := deploymentAccessConfig{
-		FlowName:              flowName,
-		DeploymentName:        deploymentName,
-		ServiceAccountName:    serviceAccountName,
 		WorkspaceResource:     workspace,
 		WorkspaceResourceName: workspaceResourceName,
 	}
+
+	var deployment api.Deployment
+	var deploymentAccess api.DeploymentAccessControl
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV6ProviderFactories: testutils.TestAccProtoV6ProviderFactories,
@@ -81,12 +101,126 @@ func TestAccResource_deployment_access(t *testing.T) {
 			{
 				Config: fixtureAccDeploymentAccess(cfgSet),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrPair(
-						"prefect_deployment_access.test", "deployment_id",
-						"prefect_deployment.test", "id",
-					),
+					testAccCheckDeploymentExists("prefect_deployment.test", workspaceResourceName, &deployment),
+					testAccCheckDeploymentAccessExists("prefect_deployment_access.test", workspaceResourceName, &deploymentAccess),
+					testAccCheckDeploymentAccessValues(&deploymentAccess, expectedDeploymentAccessValues{
+						manageActors: []api.ObjectActorAccess{
+							{Name: serviceAccountName, Type: api.ServiceAccountAccessor},
+							{Name: teamName, Type: api.TeamAccessor},
+						},
+						runActors: []api.ObjectActorAccess{
+							{Name: serviceAccountName, Type: api.ServiceAccountAccessor},
+							{Name: teamName, Type: api.TeamAccessor},
+						},
+						viewActors: []api.ObjectActorAccess{
+							{Name: serviceAccountName, Type: api.ServiceAccountAccessor},
+							{Name: teamName, Type: api.TeamAccessor},
+						},
+					}),
 				),
 			},
 		},
 	})
+}
+
+// testAccCheckDeploymentAccessExists is a Custom Check Function that
+// verifies that the API object was created correctly.
+func testAccCheckDeploymentAccessExists(deploymentAccessResourceName string, workspaceResourceName string, deploymentAccess *api.DeploymentAccessControl) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// Get the deployment access resource we just created from the state
+		deploymentAccessResource, exists := s.RootModule().Resources[deploymentAccessResourceName]
+		if !exists {
+			return fmt.Errorf("deployment access resource not found: %s", deploymentAccessResourceName)
+		}
+		deploymentAccessID, _ := uuid.Parse(deploymentAccessResource.Primary.Attributes["deployment_id"])
+
+		// Get the workspace resource we just created from the state
+		workspaceResource, exists := s.RootModule().Resources[workspaceResourceName]
+		if !exists {
+			return fmt.Errorf("workspace resource not found: %s", workspaceResourceName)
+		}
+		workspaceID, _ := uuid.Parse(workspaceResource.Primary.ID)
+
+		// Initialize the client with the associated workspaceID
+		// NOTE: the accountID is inherited by the one set in the test environment
+		c, _ := testutils.NewTestClient()
+		deploymentAccessClient, _ := c.DeploymentAccess(uuid.Nil, workspaceID)
+
+		fetchedDeploymentAccess, err := deploymentAccessClient.Read(context.Background(), deploymentAccessID)
+		if err != nil {
+			return fmt.Errorf("error fetching deployment access: %w", err)
+		}
+
+		// Assign the fetched deployment to the passed pointer
+		// so we can use it in the next test assertion
+		*deploymentAccess = *fetchedDeploymentAccess
+
+		return nil
+	}
+}
+
+type expectedDeploymentAccessValues struct {
+	manageActors []api.ObjectActorAccess
+	runActors    []api.ObjectActorAccess
+	viewActors   []api.ObjectActorAccess
+}
+
+// testAccCheckDeploymentValues is a Custom Check Function that
+// verifies that the API object matches the expected values.
+func testAccCheckDeploymentAccessValues(fetchedDeploymentAccess *api.DeploymentAccessControl, expectedValues expectedDeploymentAccessValues) resource.TestCheckFunc {
+	return func(_ *terraform.State) error {
+		found, err := actorFound(fetchedDeploymentAccess.ManageActors, expectedValues.manageActors)
+		if err != nil {
+			return fmt.Errorf("no matching actor found: %w", err)
+		}
+		if !found {
+			return fmt.Errorf("no matching actor found: %w", err)
+		}
+
+		found, err = actorFound(fetchedDeploymentAccess.RunActors, expectedValues.runActors)
+		if err != nil {
+			return fmt.Errorf("no matching actor found: %w", err)
+		}
+		if !found {
+			return fmt.Errorf("no matching actor found: %w", err)
+		}
+
+		found, err = actorFound(fetchedDeploymentAccess.ViewActors, expectedValues.viewActors)
+		if err != nil {
+			return fmt.Errorf("no matching actor found: %w", err)
+		}
+		if !found {
+			return fmt.Errorf("no matching actor found: %w", err)
+		}
+
+		return nil
+	}
+}
+
+func actorFound(fetched []api.ObjectActorAccess, expected []api.ObjectActorAccess) (bool, error) {
+	if len(fetched) != len(expected) {
+		return false, fmt.Errorf("expected %d actors, got %d", len(fetched), len(expected))
+	}
+
+	foundNames := make([]string, len(fetched))
+	for i := range fetched {
+		foundNames[i] = fmt.Sprintf("%s:%s", fetched[i].Name, fetched[i].Type)
+	}
+
+	for i := range expected {
+		found := false
+		for j := range fetched {
+			if fetched[j].Name == expected[i].Name && fetched[j].Type == expected[i].Type {
+				found = true
+
+				break
+			}
+		}
+
+		if !found {
+			return false, fmt.Errorf("actor %s not found in %s", expected[i].Name, strings.Join(foundNames, ", "))
+		}
+	}
+
+	return true, nil
 }
