648f27663cb6dc13305c7fca97d223f8bf6ecc3a
Mitchell Nielsen <inbox@mitchn.me>
2024-11-21 17:44:34 -0600

    Add helper functions for common HTTP requests
    The HTTP requests across all of our PrefectClient-compatible clients are
    very similar, leading to a lot of duplicate code. This leaves room for
    error when adding new clients, and makes things a bit tougher to
    maintain.
    
    This change adds helper functions to replace this redundant code, coming
    with all the usual benefits of DRY-ing things up.
    
    This is also relevant as we aim for parity with the Prefect API, and the
    less code we have to write, the better!
    
    Related to https://linear.app/prefect/issue/PLA-690/cycle-6-catch-all

--- internal/client/util.go
+++ internal/client/util.go
@@ -1,6 +1,11 @@
 package client
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
@@ -57,4 +62,88 @@ func setDefaultHeaders(request *http.Request, apiKey string) {
 
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("Accept", "application/json")
+}
+
+// requestConfig is a configuration object for an HTTP request.
+type requestConfig struct {
+	method string
+	url    string
+	body   any
+
+	successCodes []int
+
+	apiKey string
+}
+
+// successCodesStatusOK is a convenience variable to use for the most common
+// success criteria.
+var successCodesStatusOK = []int{http.StatusOK}
+
+// request performs an HTTP request with the provided configuration.
+// It returns the response, or an error if the request fails.
+// The caller is responsible for closing the response body.
+func request(ctx context.Context, client *http.Client, cfg requestConfig) (*http.Response, error) {
+	var body io.Reader
+
+	if cfg.body != nil {
+		var buf bytes.Buffer
+		if err := json.NewEncoder(&buf).Encode(cfg.body); err != nil {
+			return nil, fmt.Errorf("failed to encode body data: %w", err)
+		}
+
+		body = &buf
+	} else {
+		body = http.NoBody
+	}
+
+	req, err := http.NewRequestWithContext(ctx, cfg.method, cfg.url, body)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+
+	setDefaultHeaders(req, cfg.apiKey)
+
+	// Body will be closed by the caller.
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("http error: %w", err)
+	}
+
+	for _, successCode := range cfg.successCodes {
+		if resp.StatusCode != successCode {
+			errorBody, _ := io.ReadAll(resp.Body)
+
+			return nil, fmt.Errorf("status code %s, error=%s", resp.Status, errorBody)
+		}
+	}
+
+	return resp, nil
+}
+
+// decodeResponseBody decodes the response body into the target object.
+// It returns an error if the decoding fails.
+func decodeResponseBody(respBody io.ReadCloser, target any) error {
+	if err := json.NewDecoder(respBody).Decode(target); err != nil {
+		return fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return nil
+}
+
+// requestWithDecodeResponse performs an HTTP request with the provided configuration,
+// and decodes the response body into the target object.
+// It returns an error if the request fails or the decoding fails.
+func requestWithDecodeResponse(ctx context.Context, client *http.Client, cfg requestConfig, target any) error {
+	resp, err := request(ctx, client, cfg)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if err := decodeResponseBody(resp.Body, target); err != nil {
+		return fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return nil
 }
