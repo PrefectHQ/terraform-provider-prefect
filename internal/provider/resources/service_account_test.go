91c0834e85eb12703746d5a11dc673469a35537e
Edward Park <ed.sh.park@gmail.com>
2024-12-13 16:52:29 -0800

    fix(service-accounts): allow for rotating forever keys

--- internal/provider/resources/service_account_test.go
+++ internal/provider/resources/service_account_test.go
@@ -56,6 +56,16 @@ resource "prefect_service_account" "bot" {
 }`, name, expiration.Format(time.RFC3339))
 }
 
+func fixtureAccServiceAccountResourceKeyKeepers(name string, keeperValue string) string {
+	return fmt.Sprintf(`
+resource "prefect_service_account" "bot" {
+	name = "%s"
+	api_key_keepers = {
+	  foo = "%s"
+	}
+}`, name, keeperValue)
+}
+
 func fixtureAccServiceAccountResourceUpdateAccountRoleName(name string, roleName string) string {
 	return fmt.Sprintf(`
 resource "prefect_service_account" "bot" {
@@ -92,12 +102,12 @@ func TestAccResource_service_account(t *testing.T) {
 			},
 			{
 				// Ensure non-expiration time change DOESN'T trigger a key rotation
-				Config: fixtureAccServiceAccountResource(botRandomName),
+				Config: fixtureAccServiceAccountResource(botRandomName2),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckServiceAccountResourceExists(botResourceName, &bot),
-					testAccCheckServiceAccountValues(&bot, &api.ServiceAccount{Name: botRandomName, AccountRoleName: "Member"}),
+					testAccCheckServiceAccountValues(&bot, &api.ServiceAccount{Name: botRandomName2, AccountRoleName: "Member"}),
 					testAccCheckServiceAccountAPIKeyUnchanged(botResourceName, &apiKey),
-					resource.TestCheckResourceAttr(botResourceName, "name", botRandomName),
+					resource.TestCheckResourceAttr(botResourceName, "name", botRandomName2),
 				),
 			},
 			{
@@ -108,6 +118,36 @@ func TestAccResource_service_account(t *testing.T) {
 					testAccCheckServiceAccountValues(&bot, &api.ServiceAccount{Name: botRandomName, AccountRoleName: "Member"}),
 					testAccCheckServiceAccountAPIKeyRotated(botResourceName, &apiKey),
 					resource.TestCheckResourceAttr(botResourceName, "name", botRandomName),
+				),
+			},
+			{
+				// Ensure that switching to key keepers DOES trigger a key rotation
+				Config: fixtureAccServiceAccountResourceKeyKeepers(botRandomName, "keeper-value-1"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceAccountResourceExists(botResourceName, &bot),
+					testAccCheckServiceAccountValues(&bot, &api.ServiceAccount{Name: botRandomName, AccountRoleName: "Member"}),
+					testAccCheckServiceAccountAPIKeyRotated(botResourceName, &apiKey),
+					resource.TestCheckResourceAttr(botResourceName, "name", botRandomName),
+				),
+			},
+			{
+				// Ensure that key keepers change DOES trigger a key rotation
+				Config: fixtureAccServiceAccountResourceKeyKeepers(botRandomName, "keeper-value-2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceAccountResourceExists(botResourceName, &bot),
+					testAccCheckServiceAccountValues(&bot, &api.ServiceAccount{Name: botRandomName, AccountRoleName: "Member"}),
+					testAccCheckServiceAccountAPIKeyRotated(botResourceName, &apiKey),
+					resource.TestCheckResourceAttr(botResourceName, "name", botRandomName),
+				),
+			},
+			{
+				// Ensure that a non-key keeper change DOES NOT trigger a key rotation
+				Config: fixtureAccServiceAccountResourceKeyKeepers(botRandomName2, "keeper-value-2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceAccountResourceExists(botResourceName, &bot),
+					testAccCheckServiceAccountValues(&bot, &api.ServiceAccount{Name: botRandomName2, AccountRoleName: "Member"}),
+					testAccCheckServiceAccountAPIKeyUnchanged(botResourceName, &apiKey),
+					resource.TestCheckResourceAttr(botResourceName, "name", botRandomName2),
 				),
 			},
 			{
@@ -202,6 +242,8 @@ func textAccCheckServiceAccountAPIKeyStored(resourceName string, passedKey *stri
 	}
 }
 
+// testAccCheckServiceAccountAPIKeyUnchanged is a helper function that checks if the API key was unchanged.
+// Upon success, it will ensure that the passeKey is updated to the state key (which is a no-op).
 func testAccCheckServiceAccountAPIKeyUnchanged(n string, passedKey *string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		// find the corresponding state object
@@ -215,11 +257,14 @@ func testAccCheckServiceAccountAPIKeyUnchanged(n string, passedKey *string) reso
 		if *passedKey != key {
 			return fmt.Errorf("key was incorrectly rotated, since the old key=%s is different from new key=%s", *passedKey, key)
 		}
+		*passedKey = key
 
 		return nil
 	}
 }
 
+// testAccCheckServiceAccountAPIKeyRotated is a helper function that checks if the API key was rotated correctly.
+// Upon success, it will ensure that the passeKey is updated to the state key (which is new).
 func testAccCheckServiceAccountAPIKeyRotated(n string, passedKey *string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -232,6 +277,7 @@ func testAccCheckServiceAccountAPIKeyRotated(n string, passedKey *string) resour
 		if *passedKey == key {
 			return fmt.Errorf("key rotation did not occur correctly, as the old key=%s is the same as the new key=%s", *passedKey, key)
 		}
+		*passedKey = key
 
 		return nil
 	}
