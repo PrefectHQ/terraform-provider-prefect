a94b0c088f5b5b2e3fcfffd0b194778a5d0e3a32
Edward Park <ed.sh.park@gmail.com>
2024-12-26 09:12:19 -0800

    words

--- internal/client/getters.go
+++ internal/client/getters.go
@@ -1,6 +1,7 @@
 package client
 
-// GetEndpointHost returns the endpoint host.
+// GetEndpointHost returns the endpoint host,
+// which is the API domain without the trailing subpath.
 // eg. https://api.prefect.cloud
 func (c *Client) GetEndpointHost() string {
 	return c.endpointHost
