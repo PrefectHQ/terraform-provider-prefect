7a71aa0a611d33506e0c37b2b014ca10ec86576f
jimid27 <jimi@prefect.io>
2025-01-31 15:42:42 -0500

    Add example

---
+++ examples/resources/prefect_global_concurrency_limit/resource.tf
@@ -0,0 +1,14 @@
+provider "prefect" {}
+
+data "prefect_workspace" "test" {
+  handle = "my-workspace"
+}
+
+resource "prefect_global_concurrency_limit" "test" {
+  workspace_id          = data.prefect_workspace.test.id
+  name                  = "test-global-concurrency-limit"
+  concurrency_limit     = 1
+  active                = true
+  active_slots          = 0
+  slot_decay_per_second = 1.5
+}
