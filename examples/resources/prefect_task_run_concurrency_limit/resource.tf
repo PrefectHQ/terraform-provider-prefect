77a92d50615def5dd680f1717cfd0cc8da962cdd
jimid27 <jimi@prefect.io>
2025-01-31 11:53:10 -0500

    Include example in doc

--- examples/resources/prefect_task_run_concurrency_limit/resource.tf
+++ examples/resources/prefect_task_run_concurrency_limit/resource.tf
@@ -10,4 +10,21 @@ resource "prefect_task_run_concurrency_limit" "test" {
   tag               = "test-tag"
 }
 
-# Example of how to reference this resources is in the `examples/resources/prefect_task_run_concurrency_limit/task-run-concurrency.py` file
+# Example of a task that will be limited to 1 concurrent run:
+/*
+from prefect import flow, task
+
+# This task will be limited to 1 concurrent run
+@task(tags=["test-tag"])
+def my_task():
+    print("Hello, I'm a task")
+
+
+@flow
+def my_flow():
+    my_task()
+
+
+if __name__ == "__main__":
+    my_flow()
+*/
