e000f8c62957ff9e4c9099918567999c2538427a
Mitchell Nielsen <inbox@mitchn.me>
2025-03-20 12:01:16 -0500

    Add comment on how to find plan support info

--- internal/provider/helpers/plans.go
+++ internal/provider/helpers/plans.go
@@ -29,6 +29,17 @@ var (
 
 // DescriptionWithPlans adds a note to the description denoting which plans
 // the resource or datasource supports.
+//
+// This function will add a note to the description denoting which plans
+// the resource or datasource supports.
+//
+// Plan support information can be found in a few ways:
+// - in the product docs: https://docs.prefect.io
+// - in the product plans page: https://www.prefect.io/pricing
+// - in the "Account settings" page on the left panel
+// - in an instance of the OSS UI, manually finding which features are available
+//
+// This function is most often used in the `Schema` method of a resource or datasource.
 func DescriptionWithPlans(description string, plans ...string) string {
 	plansFormatted := strings.Join(plans, ", ")
 
